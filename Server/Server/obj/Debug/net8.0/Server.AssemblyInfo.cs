c515070bc6707a0b2c8072b2a6bbb371383621e3
YoonJungOh <atcnature@gmail.com>
2026-03-31 19:25:46 +0900

    [fix] 근접 공격 버그 해결

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58b8f349b15036866733cfc9a2887c17e2b8a05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb231ef00c566728e06f5b3e88dd81ca391ea2e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
