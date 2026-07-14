f2f7b6754939fb5bc428b275ae7980cd0a2420f6
Yoonjungoh <your_email@example.com>
2026-07-14 16:30:55 +0900

    [fix] ci/cd yml 수정 후 테스트용 push

--- Server/Server/RedisManager.cs
+++ Server/Server/RedisManager.cs
@@ -7,7 +7,7 @@ namespace Server
         public static RedisManager Instance { get; } = new RedisManager();
 
         private ConnectionMultiplexer _multiplexer;
-
+ 
         private RedisManager() { }
  
         public void Init(string configuration = "localhost:6379")
